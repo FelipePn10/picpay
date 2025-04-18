d4514270824888371c77712ecc1aec47cd59a44c
Felipe Panosso <felipepanosso21@gmail.com>
2025-04-18 11:56:03 -0300

    feat: Entity mapping

---
+++ src/main/java/entity/WalletType.java
@@ -0,0 +1,32 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_wallet_type")
+public class WalletType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public WalletType() {
+    }
+
+    public WalletType(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
